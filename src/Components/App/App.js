7815e1d0185a15048489c44bc5b57715e0d6d9e2
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2022-01-14 23:37:16 -0700

    Add blog and report calls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,33 @@
 import './App.scss';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../svg/logo.svg';
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [reports, setReports] = useState([]);
 
   useEffect(() => {
-  fetchData('https://api.spaceflightnewsapi.net/v3/articles')
+  fetchData( 'https://api.spaceflightnewsapi.net/v3/articles', setArticles )
+  fetchData( 'https://api.spaceflightnewsapi.net/v3/blogs', setBlogs )
+  fetchData( 'https://api.spaceflightnewsapi.net/v3/reports', setReports )
   }, [])
 
-  const fetchData = async ( api ) => {
+  const fetchData = async ( api, setState ) => {
     try {
       const res = await fetch( api )
       const resJson = await res.json()
-      console.log(resJson)
-    } catch(err) {
-      console.log(err.stack)
+      setState( resJson )
+    } 
+    catch( err ) {
+      console.log( err )
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
